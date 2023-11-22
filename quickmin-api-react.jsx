869c64870cbaa8f87f9cae843f754d0384839c1b
Mikael Lindqvist <li.mikael@gmail.com>
2023-11-22 11:11:07 +0800

    initial

---
+++ quickmin-api-react.jsx
@@ -0,0 +1,18 @@
+import {createContext, useContext} from "react";
+import {QuickminApi} from "./api.js"
+
+let QuickminApiContext=createContext();
+
+export function QuickminApiProvider({fetch, url, apiKey, headers, children}) {
+	let api=new QuickminApi({fetch, url, apiKey, headers});
+
+	return (<>
+		<QuickminApiContext.Provider value={api}>
+			{children}
+		</QuickminApiContext.Provider>
+	</>);
+}
+
+export function useQuickminApi() {
+	return useContext(QuickminApiContext);
+}
